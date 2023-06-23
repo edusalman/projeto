71099c3333ff443b3bdebe660c17c41aab0db9fc
Eduardo Salman <ehnsalman@gmail.com>
2023-06-23 20:12:01 -0300

    alterção de fotos e links testes para os reais

--- script.js
+++ script.js
@@ -4,14 +4,8 @@ function toggleMode() {
 
     const img = document.querySelector("#profile img")
 
-    if(html.classList.contains("light")) {
-        img.setAttribute("src", "./images/curintia-light.png")
-    } else {
-        img.setAttribute("src", "./images/curintia-dark.png")
-
     }
 
-}
 // modo de fazer o toggle funcionar manualmente, serve pra deixar a pagina no dark ou light mode
 //        if(html.classList.contains('light')) {
 //            html.classList.remove('light')
